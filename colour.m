a18de4b9a2a4ec462c535fa0532dfc13d5c68253
Thomas Fortin <thomas@altislabs.com>
2021-12-06 20:18:01 -0500

    finished colour segmentation

--- colour.m
+++ colour.m
@@ -10,23 +10,27 @@ f_lab = applycform(f_rgb, transform);
 % title('L*a*b* colour space')
 
 
-% K = 2;
-% row = [55 200];
-% col = [155 400];
-K = 4;
-row = [55 130 200 280];
-col = [155 110 400 470];
+K = 2;
+row = [55 200];
+col = [155 400];
+% K = 4;
+% row = [55 130 200 280];
+% col = [155 110 400 470];
+
 % Convert (r,c) indexing to 1D linear indexing.
-idx = sub2ind([size(f,1) size(f,2)], row, col);
+idx = sub2ind([size(f_lab,1) size(f_lab,2)], row, col);
+
+ab = double(f_lab(:,:,2:3)); % NOT im2double
+m = size(ab,1);
+n = size(ab,2);
+ab = reshape(ab,m*n,2);
+
 % Vectorize starting coordinates
 for k = 1:K
     mu(k,:) = ab(idx(k),:);
 end
 
-ab = double(im_lab(:,:,2:3)); % NOT im2double
-m = size(ab,1);
-n = size(ab,2);
-ab = reshape(ab,m*n,2);
+cluster_idx = kmeans(ab, [], 'Start', mu);
 
 % Label each pixel according to k-means
 pixel_labels = reshape(cluster_idx, m, n);
@@ -34,37 +38,16 @@ h = figure,imshow(pixel_labels, [])
 title('Image labeled by cluster index');
 colormap('jet')
 
-=======
-img = imread('peppers.png');
-ycbcr = rgb2ycbcr(img);
-y = ycbcr(:,:,1);
-cb = ycbcr(:,:,2);
-cr = ycbcr(:,:,3);
+for k = 1:K
+    img = zeros(size(f_rgb));
+    mask = pixel_labels==k
+    img(mask(:, :, [1 1 1])) = f_rgb(mask(:, :, [1 1 1]));
+    figure()
+    imshow(uint8(img));
+end
 
 
-figure()
-imshow(img)
 
-figure()
-imshow(y)
 
-figure()
-imshow(cb)
 
-figure()
-imshow(cr)
-
-new_cb = imresize(imresize(cb, .5, 'bilinear'), 2, 'bilinear');
-new_cr = imresize(imresize(cr, .5, 'bilinear'), 2, 'bilinear');
-
-new_ycbcr = zeros(size(ycbcr));
-new_ycbcr(:,:,1) = y;
-new_ycbcr(:,:,2) = new_cb;
-new_ycbcr(:,:,3) = new_cr;
-new_ycbcr = uint8(new_ycbcr);
-
-new_img = ycbcr2rgb(new_ycbcr);
-
-figure()
-imshow(new_img)
 
--- subsampling.m
+++ subsampling.m
@@ -30,3 +30,16 @@ new_img = ycbcr2rgb(new_ycbcr);
 
 figure()
 imshow(new_img)
+
+new_y = imresize(imresize(y, .5, 'bilinear'), 2, 'bilinear');
+
+new_ycbcr = zeros(size(ycbcr));
+new_ycbcr(:,:,1) = new_y;
+new_ycbcr(:,:,2) = cb;
+new_ycbcr(:,:,3) = cr;
+new_ycbcr = uint8(new_ycbcr);
+
+new_img = ycbcr2rgb(new_ycbcr);
+
+figure()
+imshow(new_img)
