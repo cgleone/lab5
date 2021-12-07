6a96c96d59e6d7416ba04ea0288214677171fd91
cgleone <sealeone8120@gmail.com>
2021-12-06 19:12:42 -0500

    thomas's work on q1

---
+++ subsampling.m
@@ -0,0 +1,32 @@
+img = imread('peppers.png');
+ycbcr = rgb2ycbcr(img);
+y = ycbcr(:,:,1);
+cb = ycbcr(:,:,2);
+cr = ycbcr(:,:,3);
+
+
+figure()
+imshow(img)
+
+figure()
+imshow(y)
+
+figure()
+imshow(cb)
+
+figure()
+imshow(cr)
+
+new_cb = imresize(imresize(cb, .5, 'bilinear'), 2, 'bilinear');
+new_cr = imresize(imresize(cr, .5, 'bilinear'), 2, 'bilinear');
+
+new_ycbcr = zeros(size(ycbcr));
+new_ycbcr(:,:,1) = y;
+new_ycbcr(:,:,2) = new_cb;
+new_ycbcr(:,:,3) = new_cr;
+new_ycbcr = uint8(new_ycbcr);
+
+new_img = ycbcr2rgb(new_ycbcr);
+
+figure()
+imshow(new_img)
